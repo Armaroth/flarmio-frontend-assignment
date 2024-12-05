b0234c950ceec0dbe47d7af0b974c62484191da0
Stefanos <spalaio@moby-it.com>
2024-12-05 15:45:00 +0200

    Js implemmentation

--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
-console.log('hello world');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const warning = document.querySelector('#warning')
+
+input?.addEventListener('input', (event: Event) => {
+    event.preventDefault();
+    validateInput(input.value.trim())
+})
+
+function validateInput(userInput: any) {
+    if (!userInput) {
+        warning!.innerHTML = ''
+        return
+    }
+    if (+userInput && +userInput > 10) {
+        button!.disabled = false;
+        button?.classList.remove('disabled');
+        warning!.innerHTML = ''
+    }
+    else {
+        button!.disabled = true;
+        button?.classList.add('disabled');
+        warning!.innerHTML = '<span>Acceptable input: numbers >10</span/>'
+
+    }
+}
+form?.addEventListener('submit', (event: SubmitEvent) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const numberValue = formData.get('userInput') as string;
+    console.log('Form submitted with user input: ', numberValue)
+})
